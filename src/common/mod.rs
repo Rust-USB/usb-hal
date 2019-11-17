4a910be042f5a8465c0e85d175147c43dcfd0828
Boris Vinogradov <no111u3@gmail.com>
2019-11-17 12:23:55 +0300

    Create skeleton of common part

---
+++ src/common/mod.rs
@@ -0,0 +1,9 @@
+//! Common part of usb hal
+//!
+//! Main features of module are used in this package
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn none() {}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 #![no_std]
 #![warn(missing_docs)]
 
+pub mod common;
+
 #[cfg(test)]
 mod tests {
     #[test]
